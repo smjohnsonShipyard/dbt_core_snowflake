34b8f083680bef9308249e5949865c2bc589dc8e
Steven <steven.johnson@shipyardapp.com>
2022-05-24 18:23:47 -0400

    changed starting location

---
+++ models/538_football/staging/stg_football_matches.sql
@@ -0,0 +1,10 @@
+--Redshift
+select * from soccer.stg_football_matches
+
+--BQ
+SELECT * FROM `dbt-demos.538_football.stg_football_matches`
+
+--Snowflake
+select * from "DBT_HOL_DEV"."PUBLIC"."STG_FOOTBALL_MATCHES"
+
+--Databricks
---
+++ models/538_football/staging/stg_football_rankings.sql
@@ -0,0 +1,8 @@
+--Redshift
+select * from soccer.stg_football_rankings
+
+--BQ
+SELECT * FROM `dbt-demos.538_football.stg_football_rankings`
+
+--Snowflake
+select * from "DBT_HOL_DEV"."PUBLIC"."stg_football_rankings"
