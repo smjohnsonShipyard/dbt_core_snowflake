60a587ece35255fcd8407716cbd28b0443def7bd
Steven <steven.johnson@shipyardapp.com>
2022-05-25 14:44:26 -0400

    initial commit

--- models/538_football/staging/stg_football_rankings.sql
+++ models/538_football/staging/stg_football_rankings.sql
@@ -1,8 +1,1 @@
---Redshift
-select * from soccer.stg_football_rankings
-
---BQ
-SELECT * FROM `dbt-demos.538_football.stg_football_rankings`
-
---Snowflake
 select * from "DBT_HOL_DEV"."PUBLIC"."stg_football_rankings"
